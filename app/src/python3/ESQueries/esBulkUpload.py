98d29a58e41ab683ee2be90acbde8d0123095844
Diablo73 <ashul2173@gmail.com>
2022-12-09 23:51:12 +0530

    logs for upload

--- app/src/python3/ESQueries/esBulkUpload.py
+++ app/src/python3/ESQueries/esBulkUpload.py
@@ -1,17 +1,22 @@
 import os
 import json
 import requests
+import pytz
+from datetime import datetime
 
-filePaths = ["app/src/resources/students_30k_bulkES.txt", "../../resources/students_30k_bulkES.txt"]
+
+recordsFilePaths = ["app/src/resources/students_30k_bulkES.txt", "../../resources/students_30k_bulkES.txt"]
+logsFilePaths = ["app/src/logs/esBulkUploadLogs.txt", "../../logs/esBulkUploadLogs.txt"]
+
 
 def bulkUpload():
 	records = ""
-	for path in filePaths:
+	for path in recordsFilePaths:
 		try:
-			studentDataTxtFile = open(path, "r")
-			records = studentDataTxtFile.read()
+			recordsTxtFile = open(path, "r")
+			records = recordsTxtFile.read()
 			print("Data found in file : " + path)
-			studentDataTxtFile.close()
+			recordsTxtFile.close()
 			break
 		except Exception as e:
 			print(str(e))
@@ -20,6 +25,37 @@ def bulkUpload():
 		records += "\n"
 		response = requests.post(os.getenv("ES_URL") + "/_bulk", data=records,
 		                         headers={"content-type": "application/json", "charset": "UTF-8"}).json()
-		return "Upload query execution : Success!!! ✅\n" + str(response)
+		return "Upload query execution : Success!!! ✅\n" + str(writeLogs(response))
 	except Exception as e:
 		return "Upload query execution : Fail!!! ❌\n" + str(e)
+
+
+def writeLogs(response):
+	header = ("#" * 15) + "  " + str(datetime.now(pytz.timezone("Asia/Kolkata"))) + "  " + ("#" * 15) + "\n\n"
+	summary = ""
+	logs = ""
+	createdCount = 0
+	errorCount = 0
+	logsTxtFile = None
+	for path in logsFilePaths:
+		try:
+			logsTxtFile = open(path, "w")
+			break
+		except Exception as e:
+			print(str(e))
+
+	if not response["errors"]:
+		createdCount = len(response["items"])
+		summary = json.dumps({"createdCount": createdCount, "errorCount": errorCount})
+	else:
+		for doc in response["items"]:
+			if doc["create"]["status"] == 201:
+				createdCount += 1
+			else:
+				errorCount += 1
+				logs += json.dumps(doc["create"]) + "\n"
+		summary = json.dumps({"createdCount": createdCount, "errorCount": errorCount}) + "\n\n"
+	logs = header + summary + logs
+	logsTxtFile.write(logs)
+	logsTxtFile.close()
+	return summary

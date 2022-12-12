9924f3ca6dc449d2b76977ee9024413caf561df4
Diablo73 <ashul2173@gmail.com>
2022-12-12 18:19:34 +0530

    searchQueryUsingLogicalAND

--- app/src/python3/ESQueries/esDocSearch.py
+++ app/src/python3/ESQueries/esDocSearch.py
@@ -13,6 +13,8 @@ def printOptions():
 		"",
 		"1: Search a document using documentId",
 		"2: Search a document using a keyWord",
+		"3: Search query using only logical AND",
+		"4: Search query using only logical OR",
 		"0: Quit",
 		""
 	]
@@ -28,10 +30,18 @@ def searchES():
 			response = searchADocumentUsingDocumentId()
 		elif i == "2":
 			response = searchADocumentUsingKeyWord()
+		elif i == "3":
+			response = searchQueryUsingLogicalAND()
+		elif i == "4":
+			response = "Not yet implemented"
 		else:
 			break
 		print(response)
 	return ("#" * 15) + " Search is Over!!! " + ("#" * 15) + "\n\n"
+
+
+def searchESAPIWithJSONBody(jsonBody):
+	return requests.get(os.getenv("ES_URL") + "/" + os.getenv("ES_INDEX") + "/_search", json=jsonBody).json()
 
 
 def searchADocumentUsingDocumentId():
@@ -47,9 +57,37 @@ def searchADocumentUsingKeyWord():
 	if keyWord == "":
 		keyWord = random.choice(random.choice(recordsUtils.getKeyWordList()))
 		print("keyWord : " + keyWord)
-	searchQueryBody = {"size": MAX_NUMBER_OF_RECORDS, "query": {"simple_query_string": {"query": keyWord}}}
-	return requests.get(os.getenv("ES_URL") + "/" + os.getenv("ES_INDEX") + "/_search", json=searchQueryBody).json()
+	searchQueryBody = {"size": MAX_NUMBER_OF_RECORDS, "query": getFilterOptionQuery(keyWord)}
+	return searchESAPIWithJSONBody(searchQueryBody)
+
+
+def searchQueryUsingLogicalAND():
+	filterQuery = getFilterQuery()
+	searchQueryBody = {"query": {"bool": {"filter": filterQuery}}}
+	return searchESAPIWithJSONBody(searchQueryBody)
 
 
 def getRandomDocumentId():
 	return random.choice(recordsUtils.getRecords())["id"]
+
+
+def getFilterQuery():
+	filterQuery = []
+	print("Search based on logical AND")
+	print("Enter keyWords to search and then press enter to search")
+	print("Press enter for random existing (3 keyWords chosen randomly)")
+	while True:
+		keyword = input("Enter keyWord " + str(len(filterQuery) + 1) + " : ")
+		if keyword == "":
+			break
+		filterQuery += [getFilterOptionQuery(keyword)]
+	if len(filterQuery) == 0:
+		keyWordList = recordsUtils.getKeyWordList()
+		filterQuery += [getFilterOptionQuery(random.choice(keyWordList[0]))]
+		filterQuery += [getFilterOptionQuery(random.choice(random.choice(keyWordList[1:4])))]
+		filterQuery += [getFilterOptionQuery(random.choice(keyWordList[4]))]
+	return filterQuery
+
+
+def getFilterOptionQuery(keyword):
+	return {"simple_query_string": {"query": keyword}}

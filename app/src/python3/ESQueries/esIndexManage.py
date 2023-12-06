a4b3c1dee0c55f3c2bf2b19d2133a1a52ef9df9a
Diablo73 <ashul2173@gmail.com>
2023-12-06 14:57:08 +0530

    Get Index Size

--- app/src/python3/ESQueries/esIndexManage.py
+++ app/src/python3/ESQueries/esIndexManage.py
@@ -8,7 +8,8 @@ def printOptions():
 	table = prettytable.PrettyTable(["Option", "Event"])
 	options = [
 		[1, "Add Index"],
-		[2, "Delete Index"],
+		[2, "Get Index Size"],
+		[3, "Delete Index"],
 		[0, "Quit"]
 	]
 	table.add_rows(options)
@@ -21,6 +22,8 @@ def manageIndex():
 		if i == "1":
 			response = putNewIndexInES()
 		elif i == "2":
+			response = getIndexSizeInES()
+		elif i == "3":
 			response = deleteIndexInES()
 		else:
 			break
@@ -29,6 +32,15 @@ def manageIndex():
 def putNewIndexInES():
 	return requests.put(os.getenv("ES_URL") + "/" + os.getenv("ES_INDEX")).json()
 
+def getIndexSizeInES():
+	# can also get in csv format or in mb or gb and use wildcard in index name
+	# if index names were student_2023-12-06, student_2023-12-07, etc
+	# /_cat/indices?h=index,store.size&bytes=mb&format=csv&s=index&index=student_2023-*
+	return [{"unit" : "kb"}] + requests.get(os.getenv("ES_URL")
+	                                        + "/_cat/indices?h=index,store.size&bytes=kb&format=json&s=index&index="
+	                                        + os.getenv("ES_INDEX")).json()
+
+
 def createNewIndexPatternInES():
 	return requests.post(os.getenv("ES_URL") + "/" + os.getenv("ES_INDEX") + "/index_patterns/index_pattern",
 	                     json={ "index_pattern": { "title": os.getenv("ES_INDEX") } }).json()
@@ -36,8 +48,3 @@ def createNewIndexPatternInES():
 
 def deleteIndexInES():
 	return requests.delete(os.getenv("ES_URL") + "/" + os.getenv("ES_INDEX")).json()
-
-
-deleteQueryBody = '{"query":{"match_all":{}}}'
-response = requests.post(os.getenv("ES_URL") + "/" + os.getenv("ES_INDEX") + "/_delete_by_query",
-                         json=json.loads(deleteQueryBody)).json()
